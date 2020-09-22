7110a2ddc9e444f8e597aafe228993b19d67253b
Igor Levin <lytghzys@gmail.com>
2020-09-22 22:49:17 +0300

    kjj

--- 404/Form1.cs
+++ 404/Form1.cs
@@ -12,41 +12,43 @@ namespace _404
 {
     public partial class Form1 : Form
     {
-        private const int V = 0b1;
+ 
         private double x1, x2, x3, z1, z2, z3;
         private double[,] arr = new double[3, 4];
+        private double[,] arr2 = new double[3, 3];
         public Form1()
         {
            
             InitializeComponent();
             Read(); // Заполняем матрицу вводными данными
+            Read2();
             SelectX(); // Находим наше Х0
+
             #region Заполнение формы нашим Х0
             valueX1.Text = x1.ToString();
             valueX2.Text = x2.ToString();
             valueX3.Text = x3.ToString();
             #endregion
-            NextX(); // Поиск Х2
+
+            
+
             #region Заполням формы нашим Х1
             valueX12.Text = z1.ToString();
             value22.Text = z2.ToString();
             value32.Text = z3.ToString();
             #endregion
-            do
+
+            for (int i = 0; i < 10; i++)
             {
                 x1 = z1;
                 x2 = z2;
                 x3 = z3;
                 NextX();
-            } while (z1 - x1 <= 0.5);
-            
+            }
+        
             valueX12.Text = z1.ToString();
             value22.Text = z2.ToString();
             value32.Text = z3.ToString();
-
-
-
-
         }
 
         public void SelectX()
@@ -57,9 +59,9 @@ namespace _404
         }
         public void NextX()
         {
-            z1 =Math.Round( x1 - ((arr[0, 1] / arr[0, 0]) * x2) - ((arr[0, 2] / arr[0, 0]) * x3),4);
-            z2= Math.Round(x2 - (arr[1, 0] / arr[1, 1]) * x1 - ((arr[1, 2] / arr[1, 1]) * x3),4);
-            z3=Math.Round(x3 - (arr[2, 0] / arr[2, 2]) * x1 - ((arr[2, 1] / arr[2, 2]) *x2),4);
+            z1 =Math.Round(arr2[0,0]-arr2[0,1]*x2-arr2[0,2]*x3,4);
+            z2= Math.Round(arr2[1,1]-arr2[1,0]*x1-arr2[1,2]*x3,4);
+            z3=Math.Round(arr2[2,2]-arr2[2,0]*x1-arr2[2,1]*x2,4);
         }
         private void Read() 
         {
@@ -75,6 +77,21 @@ namespace _404
             arr[2, 1] = double.Parse(m10.Text);
             arr[2, 2] = double.Parse(m11.Text);
             arr[2, 3] = double.Parse(m12.Text);
+        }
+        private void Read2() 
+        {
+            arr2[0, 0] = arr[0, 3] / arr[0, 0];
+            arr2[0, 1] = arr[0, 1] / arr[0, 0];
+            arr2[0, 2] = arr[0, 2] / arr[0, 0];
+            
+            arr2[1, 0] = arr[1, 0] / arr[1, 1];
+            arr2[1, 1] = arr[1, 3] / arr[1, 1];
+            arr2[1, 2] = arr[1, 2] / arr[1, 1];
+            
+            arr2[2, 0] = arr[2, 0] / arr[2, 2];
+            arr2[2, 1] = arr[2, 1] / arr[2, 2];
+            arr2[2, 2] = arr[2, 3] / arr[2, 2];
+         
         }
 
        
