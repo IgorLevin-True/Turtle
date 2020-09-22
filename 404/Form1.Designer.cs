b42c17fb6a023b48edec630c4e0f89c86c8ea97f
Igor Levin <lytghzys@gmail.com>
2020-09-22 22:14:19 +0300

    Ok

---
+++ 404/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace _404
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.m1 = new System.Windows.Forms.TextBox();
+            this.m2 = new System.Windows.Forms.TextBox();
+            this.m3 = new System.Windows.Forms.TextBox();
+            this.m4 = new System.Windows.Forms.TextBox();
+            this.m5 = new System.Windows.Forms.TextBox();
+            this.m6 = new System.Windows.Forms.TextBox();
+            this.m7 = new System.Windows.Forms.TextBox();
+            this.m8 = new System.Windows.Forms.TextBox();
+            this.m9 = new System.Windows.Forms.TextBox();
+            this.m10 = new System.Windows.Forms.TextBox();
+            this.m11 = new System.Windows.Forms.TextBox();
+            this.m12 = new System.Windows.Forms.TextBox();
+            this.valueX1 = new System.Windows.Forms.TextBox();
+            this.valueX2 = new System.Windows.Forms.TextBox();
+            this.valueX3 = new System.Windows.Forms.TextBox();
+            this.valueX12 = new System.Windows.Forms.TextBox();
+            this.value22 = new System.Windows.Forms.TextBox();
+            this.value32 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // m1
+            // 
+            this.m1.Location = new System.Drawing.Point(46, 27);
+            this.m1.Name = "m1";
+            this.m1.Size = new System.Drawing.Size(100, 20);
+            this.m1.TabIndex = 0;
+            this.m1.Text = "7,6";
+            // 
+            // m2
+            // 
+            this.m2.Location = new System.Drawing.Point(182, 27);
+            this.m2.Name = "m2";
+            this.m2.Size = new System.Drawing.Size(100, 20);
+            this.m2.TabIndex = 1;
+            this.m2.Text = "0,5";
+            // 
+            // m3
+            // 
+            this.m3.Location = new System.Drawing.Point(313, 27);
+            this.m3.Name = "m3";
+            this.m3.Size = new System.Drawing.Size(100, 20);
+            this.m3.TabIndex = 2;
+            this.m3.Text = "2,4";
+            // 
+            // m4
+            // 
+            this.m4.Location = new System.Drawing.Point(419, 27);
+            this.m4.Name = "m4";
+            this.m4.Size = new System.Drawing.Size(100, 20);
+            this.m4.TabIndex = 3;
+            this.m4.Text = "1,9";
+            // 
+            // m5
+            // 
+            this.m5.Location = new System.Drawing.Point(46, 62);
+            this.m5.Name = "m5";
+            this.m5.Size = new System.Drawing.Size(100, 20);
+            this.m5.TabIndex = 4;
+            this.m5.Text = "2,2";
+            // 
+            // m6
+            // 
+            this.m6.Location = new System.Drawing.Point(182, 62);
+            this.m6.Name = "m6";
+            this.m6.Size = new System.Drawing.Size(100, 20);
+            this.m6.TabIndex = 5;
+            this.m6.Text = "9,1";
+            // 
+            // m7
+            // 
+            this.m7.Location = new System.Drawing.Point(313, 62);
+            this.m7.Name = "m7";
+            this.m7.Size = new System.Drawing.Size(100, 20);
+            this.m7.TabIndex = 6;
+            this.m7.Text = "4,4";
+            // 
+            // m8
+            // 
+            this.m8.Location = new System.Drawing.Point(419, 62);
+            this.m8.Name = "m8";
+            this.m8.Size = new System.Drawing.Size(100, 20);
+            this.m8.TabIndex = 7;
+            this.m8.Text = "9,7";
+            // 
+            // m9
+            // 
+            this.m9.Location = new System.Drawing.Point(46, 88);
+            this.m9.Name = "m9";
+            this.m9.Size = new System.Drawing.Size(100, 20);
+            this.m9.TabIndex = 8;
+            this.m9.Text = "-1,3";
+            // 
+            // m10
+            // 
+            this.m10.Location = new System.Drawing.Point(182, 88);
+            this.m10.Name = "m10";
+            this.m10.Size = new System.Drawing.Size(100, 20);
+            this.m10.TabIndex = 9;
+            this.m10.Text = "0,2";
+            // 
+            // m11
+            // 
+            this.m11.Location = new System.Drawing.Point(313, 88);
+            this.m11.Name = "m11";
+            this.m11.Size = new System.Drawing.Size(100, 20);
+            this.m11.TabIndex = 10;
+            this.m11.Text = "5,8";
+            // 
+            // m12
+            // 
+            this.m12.Location = new System.Drawing.Point(419, 88);
+            this.m12.Name = "m12";
+            this.m12.Size = new System.Drawing.Size(100, 20);
+            this.m12.TabIndex = 11;
+            this.m12.Text = "-1,4";
+            // 
+            // valueX1
+            // 
+            this.valueX1.Location = new System.Drawing.Point(46, 149);
+            this.valueX1.Name = "valueX1";
+            this.valueX1.Size = new System.Drawing.Size(100, 20);
+            this.valueX1.TabIndex = 12;
+            // 
+            // valueX2
+            // 
+            this.valueX2.Location = new System.Drawing.Point(46, 176);
+            this.valueX2.Name = "valueX2";
+            this.valueX2.Size = new System.Drawing.Size(100, 20);
+            this.valueX2.TabIndex = 13;
+            // 
+            // valueX3
+            // 
+            this.valueX3.Location = new System.Drawing.Point(46, 203);
+            this.valueX3.Name = "valueX3";
+            this.valueX3.Size = new System.Drawing.Size(100, 20);
+            this.valueX3.TabIndex = 14;
+            // 
+            // valueX12
+            // 
+            this.valueX12.Location = new System.Drawing.Point(153, 149);
+            this.valueX12.Name = "valueX12";
+            this.valueX12.Size = new System.Drawing.Size(100, 20);
+            this.valueX12.TabIndex = 15;
+            // 
+            // value22
+            // 
+            this.value22.Location = new System.Drawing.Point(153, 175);
+            this.value22.Name = "value22";
+            this.value22.Size = new System.Drawing.Size(100, 20);
+            this.value22.TabIndex = 16;
+            // 
+            // value32
+            // 
+            this.value32.Location = new System.Drawing.Point(153, 202);
+            this.value32.Name = "value32";
+            this.value32.Size = new System.Drawing.Size(100, 20);
+            this.value32.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.value32);
+            this.Controls.Add(this.value22);
+            this.Controls.Add(this.valueX12);
+            this.Controls.Add(this.valueX3);
+            this.Controls.Add(this.valueX2);
+            this.Controls.Add(this.valueX1);
+            this.Controls.Add(this.m12);
+            this.Controls.Add(this.m11);
+            this.Controls.Add(this.m10);
+            this.Controls.Add(this.m9);
+            this.Controls.Add(this.m8);
+            this.Controls.Add(this.m7);
+            this.Controls.Add(this.m6);
+            this.Controls.Add(this.m5);
+            this.Controls.Add(this.m4);
+            this.Controls.Add(this.m3);
+            this.Controls.Add(this.m2);
+            this.Controls.Add(this.m1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox m1;
+        private System.Windows.Forms.TextBox m2;
+        private System.Windows.Forms.TextBox m3;
+        private System.Windows.Forms.TextBox m4;
+        private System.Windows.Forms.TextBox m5;
+        private System.Windows.Forms.TextBox m6;
+        private System.Windows.Forms.TextBox m7;
+        private System.Windows.Forms.TextBox m8;
+        private System.Windows.Forms.TextBox m9;
+        private System.Windows.Forms.TextBox m10;
+        private System.Windows.Forms.TextBox m11;
+        private System.Windows.Forms.TextBox m12;
+        private System.Windows.Forms.TextBox valueX1;
+        private System.Windows.Forms.TextBox valueX2;
+        private System.Windows.Forms.TextBox valueX3;
+        private System.Windows.Forms.TextBox valueX12;
+        private System.Windows.Forms.TextBox value22;
+        private System.Windows.Forms.TextBox value32;
+    }
+}
+
---
+++ 404/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _404
+{
+    public partial class Form1 : Form
+    {
+        private const int V = 0b1;
+        private double x1, x2, x3, z1, z2, z3;
+        private double[,] arr = new double[3, 4];
+        public Form1()
+        {
+           
+            InitializeComponent();
+            Read(); // Заполняем матрицу вводными данными
+            SelectX(); // Находим наше Х0
+            #region Заполнение формы нашим Х0
+            valueX1.Text = x1.ToString();
+            valueX2.Text = x2.ToString();
+            valueX3.Text = x3.ToString();
+            #endregion
+            NextX(); // Поиск Х2
+            #region Заполням формы нашим Х1
+            valueX12.Text = z1.ToString();
+            value22.Text = z2.ToString();
+            value32.Text = z3.ToString();
+            #endregion
+            do
+            {
+                x1 = z1;
+                x2 = z2;
+                x3 = z3;
+                NextX();
+            } while (z1 - x1 <= 0.5);
+            
+            valueX12.Text = z1.ToString();
+            value22.Text = z2.ToString();
+            value32.Text = z3.ToString();
+
+
+
+
+        }
+
+        public void SelectX()
+        {
+            x1 =Math.Round(arr[0,3] /arr[0,0],4);
+            x2 =Math.Round( arr[1,3] / arr[1,1],4);
+            x3 =Math.Round(arr[2,3] / arr[2,2],4);
+        }
+        public void NextX()
+        {
+            z1 =Math.Round( x1 - ((arr[0, 1] / arr[0, 0]) * x2) - ((arr[0, 2] / arr[0, 0]) * x3),4);
+            z2= Math.Round(x2 - (arr[1, 0] / arr[1, 1]) * x1 - ((arr[1, 2] / arr[1, 1]) * x3),4);
+            z3=Math.Round(x3 - (arr[2, 0] / arr[2, 2]) * x1 - ((arr[2, 1] / arr[2, 2]) *x2),4);
+        }
+        private void Read() 
+        {
+            arr[0, 0] = double.Parse(m1.Text);
+            arr[0, 1] = double.Parse(m2.Text);
+            arr[0, 2] = double.Parse(m3.Text);
+            arr[0, 3] = double.Parse(m4.Text);
+            arr[1, 0] = double.Parse(m5.Text);
+            arr[1, 1] = double.Parse(m6.Text);
+            arr[1, 2] = double.Parse(m7.Text);
+            arr[1, 3] = double.Parse(m8.Text);
+            arr[2, 0] = double.Parse(m9.Text);
+            arr[2, 1] = double.Parse(m10.Text);
+            arr[2, 2] = double.Parse(m11.Text);
+            arr[2, 3] = double.Parse(m12.Text);
+        }
+
+       
+    }
+}
