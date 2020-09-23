a11d44bd1cfe007e0e9f4110a9c91365e63509f3
Igor Levin <lytghzys@gmail.com>
2020-09-23 22:17:05 +0300

    Вроде готово

--- 404/Form1.Designer.cs
+++ 404/Form1.Designer.cs
@@ -40,163 +40,264 @@
             this.m10 = new System.Windows.Forms.TextBox();
             this.m11 = new System.Windows.Forms.TextBox();
             this.m12 = new System.Windows.Forms.TextBox();
-            this.valueX1 = new System.Windows.Forms.TextBox();
-            this.valueX2 = new System.Windows.Forms.TextBox();
-            this.valueX3 = new System.Windows.Forms.TextBox();
             this.valueX12 = new System.Windows.Forms.TextBox();
             this.value22 = new System.Windows.Forms.TextBox();
             this.value32 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // m1
             // 
-            this.m1.Location = new System.Drawing.Point(46, 27);
+            this.m1.BackColor = System.Drawing.Color.AliceBlue;
+            this.m1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.m1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)), true);
+            this.m1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.m1.Location = new System.Drawing.Point(14, 11);
             this.m1.Name = "m1";
-            this.m1.Size = new System.Drawing.Size(100, 20);
+            this.m1.Size = new System.Drawing.Size(60, 32);
             this.m1.TabIndex = 0;
-            this.m1.Text = "7,6";
+            this.m1.Text = "0,63";
+            this.m1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // m2
             // 
-            this.m2.Location = new System.Drawing.Point(182, 27);
+            this.m2.BackColor = System.Drawing.Color.AliceBlue;
+            this.m2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.m2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)), true);
+            this.m2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.m2.Location = new System.Drawing.Point(80, 11);
             this.m2.Name = "m2";
-            this.m2.Size = new System.Drawing.Size(100, 20);
+            this.m2.Size = new System.Drawing.Size(60, 32);
             this.m2.TabIndex = 1;
-            this.m2.Text = "0,5";
+            this.m2.Text = "0,05";
+            this.m2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // m3
             // 
-            this.m3.Location = new System.Drawing.Point(313, 27);
+            this.m3.BackColor = System.Drawing.Color.AliceBlue;
+            this.m3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.m3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)), true);
+            this.m3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.m3.Location = new System.Drawing.Point(146, 11);
             this.m3.Name = "m3";
-            this.m3.Size = new System.Drawing.Size(100, 20);
+            this.m3.Size = new System.Drawing.Size(60, 32);
             this.m3.TabIndex = 2;
-            this.m3.Text = "2,4";
+            this.m3.Text = "0,15";
+            this.m3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // m4
             // 
-            this.m4.Location = new System.Drawing.Point(419, 27);
+            this.m4.BackColor = System.Drawing.Color.AliceBlue;
+            this.m4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.m4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)), true);
+            this.m4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.m4.Location = new System.Drawing.Point(212, 11);
             this.m4.Name = "m4";
-            this.m4.Size = new System.Drawing.Size(100, 20);
+            this.m4.Size = new System.Drawing.Size(60, 32);
             this.m4.TabIndex = 3;
-            this.m4.Text = "1,9";
+            this.m4.Text = "0,34";
+            this.m4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // m5
             // 
-            this.m5.Location = new System.Drawing.Point(46, 62);
+            this.m5.BackColor = System.Drawing.Color.AliceBlue;
+            this.m5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.m5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)), true);
+            this.m5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.m5.Location = new System.Drawing.Point(14, 49);
             this.m5.Name = "m5";
-            this.m5.Size = new System.Drawing.Size(100, 20);
+            this.m5.Size = new System.Drawing.Size(60, 32);
             this.m5.TabIndex = 4;
-            this.m5.Text = "2,2";
+            this.m5.Text = "0,05";
+            this.m5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // m6
             // 
-            this.m6.Location = new System.Drawing.Point(182, 62);
+            this.m6.BackColor = System.Drawing.Color.AliceBlue;
+            this.m6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.m6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)), true);
+            this.m6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.m6.Location = new System.Drawing.Point(80, 49);
             this.m6.Name = "m6";
-            this.m6.Size = new System.Drawing.Size(100, 20);
+            this.m6.Size = new System.Drawing.Size(60, 32);
             this.m6.TabIndex = 5;
-            this.m6.Text = "9,1";
+            this.m6.Text = "0,34";
+            this.m6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // m7
             // 
-            this.m7.Location = new System.Drawing.Point(313, 62);
+            this.m7.BackColor = System.Drawing.Color.AliceBlue;
+            this.m7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.m7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)), true);
+            this.m7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.m7.Location = new System.Drawing.Point(146, 49);
             this.m7.Name = "m7";
-            this.m7.Size = new System.Drawing.Size(100, 20);
+            this.m7.Size = new System.Drawing.Size(60, 32);
             this.m7.TabIndex = 6;
-            this.m7.Text = "4,4";
+            this.m7.Text = "0,1";
+            this.m7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // m8
             // 
-            this.m8.Location = new System.Drawing.Point(419, 62);
+            this.m8.BackColor = System.Drawing.Color.AliceBlue;
+            this.m8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.m8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)), true);
+            this.m8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.m8.Location = new System.Drawing.Point(212, 49);
             this.m8.Name = "m8";
-            this.m8.Size = new System.Drawing.Size(100, 20);
+            this.m8.Size = new System.Drawing.Size(60, 32);
             this.m8.TabIndex = 7;
-            this.m8.Text = "9,7";
+            this.m8.Text = "0,32";
+            this.m8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // m9
             // 
-            this.m9.Location = new System.Drawing.Point(46, 88);
+            this.m9.BackColor = System.Drawing.Color.AliceBlue;
+            this.m9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.m9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)), true);
+            this.m9.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.m9.Location = new System.Drawing.Point(14, 87);
             this.m9.Name = "m9";
-            this.m9.Size = new System.Drawing.Size(100, 20);
+            this.m9.Size = new System.Drawing.Size(60, 32);
             this.m9.TabIndex = 8;
-            this.m9.Text = "-1,3";
+            this.m9.Text = "0,15";
+            this.m9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // m10
             // 
-            this.m10.Location = new System.Drawing.Point(182, 88);
+            this.m10.BackColor = System.Drawing.Color.AliceBlue;
+            this.m10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.m10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)), true);
+            this.m10.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.m10.Location = new System.Drawing.Point(82, 87);
             this.m10.Name = "m10";
-            this.m10.Size = new System.Drawing.Size(100, 20);
+            this.m10.Size = new System.Drawing.Size(60, 32);
             this.m10.TabIndex = 9;
-            this.m10.Text = "0,2";
+            this.m10.Text = "0,1";
+            this.m10.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // m11
             // 
-            this.m11.Location = new System.Drawing.Point(313, 88);
+            this.m11.BackColor = System.Drawing.Color.AliceBlue;
+            this.m11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.m11.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)), true);
+            this.m11.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.m11.Location = new System.Drawing.Point(146, 87);
             this.m11.Name = "m11";
-            this.m11.Size = new System.Drawing.Size(100, 20);
+            this.m11.Size = new System.Drawing.Size(60, 32);
             this.m11.TabIndex = 10;
-            this.m11.Text = "5,8";
+            this.m11.Text = "0,7";
+            this.m11.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // m12
             // 
-            this.m12.Location = new System.Drawing.Point(419, 88);
+            this.m12.BackColor = System.Drawing.Color.AliceBlue;
+            this.m12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.m12.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)), true);
+            this.m12.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.m12.Location = new System.Drawing.Point(212, 87);
             this.m12.Name = "m12";
-            this.m12.Size = new System.Drawing.Size(100, 20);
+            this.m12.Size = new System.Drawing.Size(60, 32);
             this.m12.TabIndex = 11;
-            this.m12.Text = "-1,4";
-            // 
-            // valueX1
-            // 
-            this.valueX1.Location = new System.Drawing.Point(46, 149);
-            this.valueX1.Name = "valueX1";
-            this.valueX1.Size = new System.Drawing.Size(100, 20);
-            this.valueX1.TabIndex = 12;
-            // 
-            // valueX2
-            // 
-            this.valueX2.Location = new System.Drawing.Point(46, 176);
-            this.valueX2.Name = "valueX2";
-            this.valueX2.Size = new System.Drawing.Size(100, 20);
-            this.valueX2.TabIndex = 13;
-            // 
-            // valueX3
-            // 
-            this.valueX3.Location = new System.Drawing.Point(46, 203);
-            this.valueX3.Name = "valueX3";
-            this.valueX3.Size = new System.Drawing.Size(100, 20);
-            this.valueX3.TabIndex = 14;
+            this.m12.Text = "0,72";
+            this.m12.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // valueX12
             // 
-            this.valueX12.Location = new System.Drawing.Point(153, 149);
+            this.valueX12.Location = new System.Drawing.Point(106, 207);
             this.valueX12.Name = "valueX12";
-            this.valueX12.Size = new System.Drawing.Size(100, 20);
+            this.valueX12.Size = new System.Drawing.Size(91, 20);
             this.valueX12.TabIndex = 15;
             // 
             // value22
             // 
-            this.value22.Location = new System.Drawing.Point(153, 175);
+            this.value22.Location = new System.Drawing.Point(106, 234);
             this.value22.Name = "value22";
-            this.value22.Size = new System.Drawing.Size(100, 20);
+            this.value22.Size = new System.Drawing.Size(91, 20);
             this.value22.TabIndex = 16;
             // 
             // value32
             // 
-            this.value32.Location = new System.Drawing.Point(153, 202);
+            this.value32.Location = new System.Drawing.Point(106, 260);
             this.value32.Name = "value32";
-            this.value32.Size = new System.Drawing.Size(100, 20);
+            this.value32.Size = new System.Drawing.Size(91, 20);
             this.value32.TabIndex = 17;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.GhostWhite;
+            this.button1.Location = new System.Drawing.Point(73, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 43);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Расчёт";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FloralWhite;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(71, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 20);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "X1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FloralWhite;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(71, 234);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 20);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "X2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FloralWhite;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(71, 260);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 20);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "X3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(10, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(273, 24);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Введите корректные данные";
+            this.label4.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(294, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.value32);
             this.Controls.Add(this.value22);
             this.Controls.Add(this.valueX12);
-            this.Controls.Add(this.valueX3);
-            this.Controls.Add(this.valueX2);
-            this.Controls.Add(this.valueX1);
             this.Controls.Add(this.m12);
             this.Controls.Add(this.m11);
             this.Controls.Add(this.m10);
@@ -210,7 +311,7 @@
             this.Controls.Add(this.m2);
             this.Controls.Add(this.m1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Метод простых итераций";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -230,12 +331,14 @@
         private System.Windows.Forms.TextBox m10;
         private System.Windows.Forms.TextBox m11;
         private System.Windows.Forms.TextBox m12;
-        private System.Windows.Forms.TextBox valueX1;
-        private System.Windows.Forms.TextBox valueX2;
-        private System.Windows.Forms.TextBox valueX3;
         private System.Windows.Forms.TextBox valueX12;
         private System.Windows.Forms.TextBox value22;
         private System.Windows.Forms.TextBox value32;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- 404/Form1.cs
+++ 404/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,85 +13,81 @@ namespace _404
 {
     public partial class Form1 : Form
     {
- 
         private double x1, x2, x3, z1, z2, z3;
-        private double[,] arr = new double[3, 4];
         private double[,] arr2 = new double[3, 3];
-        public Form1()
+        private string path = "C:\\Users\\lytgh\\OneDrive\\Рабочий стол\\Информация про итерации.txt";
+        private int iteration = 0;
+
+        private void button1_Click(object sender, EventArgs e)
         {
-           
-            InitializeComponent();
-            Read(); // Заполняем матрицу вводными данными
-            Read2();
-            SelectX(); // Находим наше Х0
-
-            #region Заполнение формы нашим Х0
-            valueX1.Text = x1.ToString();
-            valueX2.Text = x2.ToString();
-            valueX3.Text = x3.ToString();
+            #region Проверка на вводные данные матрицы
+            try
+            {
+            FillArray();
+            }
+            catch (Exception)
+            {
+                label4.Visible = true;
+            }
+            #endregion
+            #region Вычисление Х - нулевого.
+            x1 = Math.Round(arr2[0, 0], 4);
+            x2 = Math.Round(arr2[1, 1], 4);
+            x3 = Math.Round(arr2[2, 2], 4);
             #endregion
 
-            
-
-            #region Заполням формы нашим Х1
-            valueX12.Text = z1.ToString();
-            value22.Text = z2.ToString();
-            value32.Text = z3.ToString();
-            #endregion
-
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 20; i++)
             {
                 x1 = z1;
                 x2 = z2;
                 x3 = z3;
                 NextX();
             }
-        
+
+            #region Заполняем ячейки на форме, нашим Х1, Х2, Х3.
             valueX12.Text = z1.ToString();
             value22.Text = z2.ToString();
             value32.Text = z3.ToString();
+            #endregion
         }
 
-        public void SelectX()
+
+        public Form1()
         {
-            x1 =Math.Round(arr[0,3] /arr[0,0],4);
-            x2 =Math.Round( arr[1,3] / arr[1,1],4);
-            x3 =Math.Round(arr[2,3] / arr[2,2],4);
+            InitializeComponent();   
         }
+
+        
         public void NextX()
         {
             z1 =Math.Round(arr2[0,0]-arr2[0,1]*x2-arr2[0,2]*x3,4);
             z2= Math.Round(arr2[1,1]-arr2[1,0]*x1-arr2[1,2]*x3,4);
             z3=Math.Round(arr2[2,2]-arr2[2,0]*x1-arr2[2,1]*x2,4);
+
+            // Записываем в файл на рабочем столе информацию про данные в каждой итерации,
+            // если файла нет - он создаётся, иначе записывает в существующий.
+            using (StreamWriter stream = new StreamWriter(path, true))
+            {
+                stream.WriteLineAsync
+                   ($" X1={z1.ToString()}, X2={z2.ToString()},X3={z3.ToString()}. Итерация - {iteration++.ToString()}. E={Math.Round(z1-x1,7)} ");
+            }
         }
-        private void Read() 
+        
+        private void FillArray() 
         {
-            arr[0, 0] = double.Parse(m1.Text);
-            arr[0, 1] = double.Parse(m2.Text);
-            arr[0, 2] = double.Parse(m3.Text);
-            arr[0, 3] = double.Parse(m4.Text);
-            arr[1, 0] = double.Parse(m5.Text);
-            arr[1, 1] = double.Parse(m6.Text);
-            arr[1, 2] = double.Parse(m7.Text);
-            arr[1, 3] = double.Parse(m8.Text);
-            arr[2, 0] = double.Parse(m9.Text);
-            arr[2, 1] = double.Parse(m10.Text);
-            arr[2, 2] = double.Parse(m11.Text);
-            arr[2, 3] = double.Parse(m12.Text);
-        }
-        private void Read2() 
-        {
-            arr2[0, 0] = arr[0, 3] / arr[0, 0];
-            arr2[0, 1] = arr[0, 1] / arr[0, 0];
-            arr2[0, 2] = arr[0, 2] / arr[0, 0];
+            arr2[0, 0] = double.Parse(m4.Text) / double.Parse(m1.Text);
+            arr2[0, 1] = double.Parse(m2.Text) / double.Parse(m1.Text);
+            arr2[0, 2] = double.Parse(m3.Text) / double.Parse(m1.Text);
             
-            arr2[1, 0] = arr[1, 0] / arr[1, 1];
-            arr2[1, 1] = arr[1, 3] / arr[1, 1];
-            arr2[1, 2] = arr[1, 2] / arr[1, 1];
+            arr2[1, 0] = double.Parse(m5.Text) / double.Parse(m6.Text);
+            arr2[1, 1] = double.Parse(m8.Text) / double.Parse(m6.Text);
+            arr2[1, 2] = double.Parse(m7.Text) / double.Parse(m6.Text);
             
-            arr2[2, 0] = arr[2, 0] / arr[2, 2];
-            arr2[2, 1] = arr[2, 1] / arr[2, 2];
-            arr2[2, 2] = arr[2, 3] / arr[2, 2];
+            arr2[2, 0] = double.Parse(m9.Text) / double.Parse(m11.Text);
+            arr2[2, 1] = double.Parse(m10.Text) / double.Parse(m11.Text);
+            arr2[2, 2] = double.Parse(m12.Text) / double.Parse(m11.Text);
+
+            label4.Visible = false;
          
         }
 
